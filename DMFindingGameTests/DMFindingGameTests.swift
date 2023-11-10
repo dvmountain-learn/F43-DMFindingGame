9eefa2dffc61e5548e0319db04ae8f6023c7daf0
SENGHORT KHEANG <senghort.rupp@gmail.com>
2023-11-10 14:59:54 -0600

    Fix error in unit test.

--- DMFindingGameTests/DMFindingGameTests.swift
+++ DMFindingGameTests/DMFindingGameTests.swift
@@ -10,12 +10,12 @@ import XCTest
 
 final class DMFindingGameTests: XCTestCase {
     
-    var viewController: DMFindingGameViewController!
+    var viewController: GameViewController!
     
     override func setUp() {
         super.setUp()
         
-        viewController = DMFindingGameViewController()
+        viewController = GameViewController()
     }
     
     override func tearDown() {
@@ -26,11 +26,12 @@ final class DMFindingGameTests: XCTestCase {
     
     func testGenerateRandomLetters() {
         for _ in 1...100 {
-            let targetLetter = viewController.letters.randomElement()!
-            viewController.targetLetter = targetLetter
+            let targetLetter = viewController.gameBrain.letters.randomElement()!
+            viewController.gameBrain.targetLetter = targetLetter
             
             let numLetters = Int.random(in: 1...12)
-            let randomLetters = viewController.generateRandomLetters(numLetters: numLetters)
+            viewController.gameBrain.newGame(numLetters: numLetters)
+            let randomLetters = viewController.gameBrain.generateRandomLetters()
             
             XCTAssertEqual(randomLetters.count, numLetters)
             XCTAssertEqual(Set(randomLetters).count, randomLetters.count)
@@ -40,23 +41,23 @@ final class DMFindingGameTests: XCTestCase {
     
     func testCalculateNewScore() {
         for _ in 1...100 {
-            let targetLetter = viewController.letters.randomElement()!
-            viewController.targetLetter = targetLetter
+            let targetLetter = viewController.gameBrain.letters.randomElement()!
+            viewController.gameBrain.targetLetter = targetLetter
             
             let randomScore = Int.random(in: 1...100)
-            viewController.score = randomScore
+            viewController.gameBrain.score = randomScore
             
             let userChoseCorrectLetter = Bool.random()
             
             if userChoseCorrectLetter {
-                viewController.calculateNewScore(selectedLetter: targetLetter)
-                XCTAssertEqual(viewController.score, randomScore + 1)
+                viewController.gameBrain.letterSelected(letter: targetLetter)
+                XCTAssertEqual(viewController.gameBrain.score, randomScore + 1)
             } else {
-                let filteredArray = viewController.letters.filter { $0 != targetLetter }
+                let filteredArray = viewController.gameBrain.letters.filter { $0 != targetLetter }
                 let randomLetter = filteredArray.randomElement()!
-                viewController.calculateNewScore(selectedLetter: randomLetter)
+                viewController.gameBrain.letterSelected(letter: randomLetter)
                 
-                XCTAssertEqual(viewController.score, randomScore)
+                XCTAssertEqual(viewController.gameBrain.score, randomScore)
             }
         }
     }
